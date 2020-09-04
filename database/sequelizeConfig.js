bd940fcc6254ef45e1cc99e5adaf247ca0fcdc42
Milos Kotlar <kotlarmilos@gmail.com>
2020-09-04 10:09:51 +0200

    Update project structure and parametrize database config

--- database/sequelizeConfig.js
+++ database/sequelizeConfig.js
@@ -1,22 +1,23 @@
 require('dotenv').config();
 const path = require('path');
 const homedir = require('os').homedir();
-const pjson = require('./package.json');
+const pjson = require('../package.json');
 
 if (!process.env.ENV) {
     // Environment not set. Use the production.
     process.env.ENV = 'development';
 }
 
-const storagePath = process.env.SEQUELIZEDB ?
-    process.env.SEQUELIZEDB :
-    path.join(homedir, `${pjson.name.substring(pjson.name.lastIndexOf('/') + 1)}`, 'system.db');
+const storagePath = process.env.DATABASE_COMMAND_EXECUTOR_PATH ?
+    process.env.DATABASE_COMMAND_EXECUTOR_PATH :
+    path.join(homedir, `${pjson.name.substring(pjson.name.lastIndexOf('/') + 1)}`, 'database', 'system.db');
 
 module.exports = {
     [process.env.ENV]: {
         database: 'main',
         host: '127.0.0.1',
         dialect: 'sqlite',
+        password: process.env.DATABASE_COMMAND_EXECUTOR_PASSWORD,
         storage: storagePath,
         migrationStorageTableName: 'sequelize_meta',
         logging: false,
