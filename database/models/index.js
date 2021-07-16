dbfcc9f2a1804d29b9754d75804704f90c349d28
vterzic <veljkot@gmail.com>
2021-07-16 13:07:47 +0200

    abandon var in favour of const/let

--- database/models/index.js
+++ database/models/index.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = require('../sequelizeConfig');
 
-var basename = path.basename(__filename);
+const basename = path.basename(__filename);
 
-var config = require(`${__dirname}/../sequelizeConfig`);
-var db = {};
-var sequelize = {};
+const db = {};
+let sequelize = {};
 
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -20,7 +20,7 @@ fs
     .readdirSync(__dirname)
     .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
     .forEach((file) => {
-        var model = sequelize.import(path.join(__dirname, file));
+        const model = sequelize.import(path.join(__dirname, file));
         db[model.name] = model;
     });
 
